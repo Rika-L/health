01465bc2b4dff1460cfcbbe542585c31a81ae415
Rika-L <2209393334@qq.com>
2025-03-22 08:49:47 +0800

    feat: initial project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ModeToggle from '@/components/mode-toggle.vue'
+import Button from '@/components/ui/button/Button.vue'
+</script>
+
+<template>
+  <Button>111</Button>
+  <ModeToggle />
+</template>
